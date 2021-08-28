cfd5591f5f81bdb77651a6128df12613cd4b5613
ka-yuya-ponta <kami.yu.goal@icloud.com>
2021-08-28 17:07:17 +0900

    second

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class LikeController extends Controller
+{
+    public function store(Request $request)
+    {
+        $post = Post::find($request->post_id);
+        $post->users()->attach($request->user_id);
+        $count = $post->users()->count();
+        $result = true;
+        return response()->json([
+            'result' => $result,
+            'count' => $count,
+        ], 201);
+    }
+    public function destroy(Request $request)
+    {
+        $post = Post::find($request->post_id);
+        $post->users()->detach($request->user_id);
+        $count = $post->users()->count();
+        $result = false;
+        return response()->json([
+            'result' => $result,
+            'count' => $count,
+        ], 200);
+    }
+    public function countlikes(Request $request)
+    {
+        $post = Post::find($request->post_id);
+        $count = $post->users()->count();
+        return response()->json([
+            'count' => $count
+        ], 200);
+    }
+    public function haslike(Request $request)
+    {
+        $post = Post::find($request->post_id);
+        $hasLikes = $post->users()->where('user_id', $request->user_id)->exists();
+        if ($hasLikes) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return response()->json([
+            'result'=>$result
+        ], 200);
+    }
+}
---
+++ database/migrations/2021_08_28_070214_post_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->bigInteger('post_id')->unsigned();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            $table->unique(['user_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
