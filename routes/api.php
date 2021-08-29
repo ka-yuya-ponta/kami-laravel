c95403d933389e56fc694072d39c504e27443a85
ka-yuya-ponta <kami.yu.goal@icloud.com>
2021-08-29 15:02:35 +0900

    third

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/v1/user', UserController::class);
+
+Route::apiResource('/v1/post', PostController::class)->only([
+    'index', 'store', 'show','destroy'
+]);
+
+Route::post('/v1/like', [LikeController::class, 'store']);
+Route::delete('/v1/like', [LikeController::class, 'destroy']);
+Route::get('/v1/like/count', [LikeController::class, 'countlikes']);
+Route::get('/v1/like', [LikeController::class, 'haslike']);
+
+Route::post('/v1/comment', [CommentController::class, 'store']);
+Route::get('/v1/comment', [CommentController::class, 'show']);
